de245e881fd790f6531ff2946c60141595acd3a8
justitroyal <95460069+VMsubhash@users.noreply.github.com>
2022-05-09 15:24:09 +0000

    10 done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -1,3 +1,12 @@
-# Dictionaries
-
-filename = "dataset/mbox-short.txt"
+fname = input("Enter file name: ")
+fh = open(fname,"r")
+c=fh.readlines()
+n=0
+for i in c.copy():
+    r=i
+    if(i[:5]=="From "):
+        k=r.split()
+        print(k[1])
+        n=n+1
+print(f"There were {n} lines in the file with From as the first word")
+fh.close()
