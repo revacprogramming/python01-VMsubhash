ea7a119ccc12f721a6988b41832d942b43082a68
justitroyal <95460069+VMsubhash@users.noreply.github.com>
2022-05-10 15:33:52 +0000

    11 done

--- ActivitySet01/problem10.py
+++ ActivitySet01/problem10.py
@@ -10,3 +10,22 @@ for i in c.copy():
         n=n+1
 print(f"There were {n} lines in the file with From as the first word")
 fh.close()
+"""
+there was an question on dictionaries but no place provided here so
+
+fname = input("Enter file name: ")
+fh = open(fname,"r")
+c=fh.readlines()
+dic={}
+for i in c.copy():
+    if(i[:5]=="From "):
+        k=i.split()
+        if(k[1] in dic):
+            dic[k[1]]+=1
+        else:
+            dic[k[1]]=1
+m=max(dic.values())
+print(list(dic.keys())[list(dic.values()).index(m)],m)
+fh.close()
+"""
+
